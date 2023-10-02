2b6accd1a3333521cdefde41cc4348628b3860e9
Hellnash <shubhamraj3417@gmail.com>
2023-10-02 23:26:00 +0530

    Circuit Breaker Configuration

--- src/main/java/com/lcwd/UserService/controllers/UserController.java
+++ src/main/java/com/lcwd/UserService/controllers/UserController.java
@@ -2,6 +2,8 @@ package com.lcwd.UserService.controllers;
 
 import com.lcwd.UserService.entities.User;
 import com.lcwd.UserService.services.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/users")
+@Slf4j
 @SuppressWarnings("unused")
 public class UserController {
 
@@ -28,8 +31,17 @@ public class UserController {
     }
 
     @GetMapping("/{userId}")
+    @CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<User> getAUser(@PathVariable String userId){
             return new ResponseEntity<>(userService.getUser(userId), HttpStatus.FOUND);
 
+    }
+
+    //run if the above API which is dependent on Rating service will fail
+    public ResponseEntity<User> ratingHotelFallback(Exception ex){
+        log.error("Dependent Services are down due to "+ex.getMessage());
+        User dummyUser = User.builder().userId("12345").name("dummy").email("dummy@fake.in")
+                             .about("This is a dummy user as the rating service is down").build();
+        return new ResponseEntity<>(dummyUser, HttpStatus.OK);
     }
 }
