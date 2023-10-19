03bc0cc38359f379e73c62c6f3dba6c0d6013564
Hellnash <shubhamraj3417@gmail.com>
2023-10-19 15:57:22 +0530

    Setting Authorization Standards on the API

--- src/main/java/com/lcwd/UserService/controllers/UserController.java
+++ src/main/java/com/lcwd/UserService/controllers/UserController.java
@@ -7,9 +7,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static org.springframework.security.authorization.AuthorityReactiveAuthorizationManager.hasAuthority;
 
 @RestController
 @RequestMapping("/users")
@@ -20,16 +23,19 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    @PreAuthorize("hasAuthority('Admin')") //only admins can create the users
     @PostMapping
     public ResponseEntity<User> createUser (@RequestBody User user){
         return new ResponseEntity<>(userService.createUser(user), HttpStatus.CREATED);
     }
 
+    @PreAuthorize("hasAuthority('Admin') || hasAuthority('Users')") //admins and users can get all users
     @GetMapping
     public ResponseEntity<List<User>> findAllUsers(){
         return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.FOUND);
     }
 
+    @PreAuthorize("hasAuthority('Admin') || hasAuthority('Users')") //admins and users can get a user
     @GetMapping("/{userId}")
     @CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<User> getAUser(@PathVariable String userId){
